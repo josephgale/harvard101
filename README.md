80a14b450a1d7385169e39083ea40ddd065ff58b
joseph <24618839+josephgale@users.noreply.github.com>
2022-02-08 21:24:46 -0600

    adding first test cases

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # harvard101
+
+This repo provides resources for completing assignments in CSCI E-101 in accordance with Harvard's Code of Conduct. https://gsas.harvard.edu/codes-conduct
---
+++ a1-test-cases.py
@@ -0,0 +1,25 @@
+# Assignment: Python Programming Assignment 1 
+# This is a list of test cases for the required user-defined function. 
+# Please inform if there is an error in this list. 
+
+listFail = [
+    'q1p0d1', 
+    'Q1pd1', 
+    'Q1p1d', 
+    'Q1p0d1 asdfR', 
+    'Q5p00003d0002', 
+    'Q5p0d0',
+    'Q0p0d0',  
+    'Q10p1d0Q1d0p1', 
+    'Q12p10d2q2p1d1Q4p1d3', 
+    'Q12p10d2p1d1Q4p1d3', 
+    'Q12d2p1d1',
+    'Q3d1p2 ', 
+    ]
+listSucceed = [
+    'Q1p1d0Q1d0p1', 
+    'Q2d1p1Q5p3d2', 
+    'Q2p1d1', 
+    'Q1p1d0Q1d0p1Q2d1p1Q5p3d2',
+    'Q10p0d10'
+]
